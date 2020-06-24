f51f10514a7dff8c21abd8176a0f8ad470f779f3
Sergey Volkov <fenixguard1993@gmail.com>
2020-06-24 21:02:00 +0300

    upload project to repo

---
+++ config.py
@@ -0,0 +1,4 @@
+API_URL = 'https://api.vk.com/method'
+API_VERSION = '5.21'
+VK_TOKEN = '958eb5d439726565e9333aa30e50e0f937ee432e927f0dbd541c541887d919a7c56f95c04217915c32008'
+FIELDS = 'sex, city, country, common_count, interests, music, movies, tv, books'
